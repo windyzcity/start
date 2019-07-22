1c67375ff46dfa07700f4f5e28842562d8a5bdd1
xklieren <965994058@qq.com>
2019-07-23 00:17:13 +0800

    start_v1

---
+++ start/src/main/java/test_my_gth/start/App.java
@@ -0,0 +1,15 @@
+package test_my_gth.start;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" +new Date());
+    }
+}
