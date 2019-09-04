9e3d693fb8a2e5c2ba1111f2c41678c190c0a39c
windyzcity <49401504+windyzcity@users.noreply.github.com>
2019-09-05 00:19:54 +0800

    Update App.java
    github changes

--- start/src/main/java/test_my_gth/start/App.java
+++ start/src/main/java/test_my_gth/start/App.java
@@ -7,7 +7,7 @@ import java.util.Date;
  *
  */
 public class App 
-{
+{//github changes
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" +new Date());
