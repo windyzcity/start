7d0950d745c86170d2070d2b48f5a4cd88175f56
xklieren <965994058@qq.com>
2019-09-05 00:22:56 +0800

    2

--- start/src/main/java/test_my_gth/start/MyTest2.java
+++ start/src/main/java/test_my_gth/start/MyTest2.java
@@ -1,10 +1,10 @@
 package test_my_gth.start;
 
-public class MyTest {
+public class MyTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HI ");//
+
 	}
 
 }
