2ea99d45acd4bac59c622cfeeb293949ad5e7d9c
xklieren <965994058@qq.com>
2019-09-28 20:10:11 +0800

    add进制

--- start/src/main/java/test_my_gth/start/App.java
+++ start/src/main/java/test_my_gth/start/App.java
@@ -13,6 +13,22 @@ public class App
     {
     	//3333333333333
         System.out.println( "Hello World!" +new Date());
+        
+        //进制
+        int two=0b111;
+        System.out.println("two="+two);
+        int eight=0111;
+        System.out.println("eight="+eight);
+        int ten=111;
+        System.out.println("ten="+ten);
+        int sixteen=0x111;
+        System.out.println("sixteen="+sixteen);
+        /*Unicode:UTF-8、UTF-16
+        1.八进制转义序列：\ + 1到3位5数字；范围'\000'~'\377'      \0：空字符
+        2.Unicode转义字符：\ u + 四个十六进制数字；0~65535       \u0000：空字符
+        */
+        System.out.println("aa\007");
+        System.out.println("aa\u0007");
     }
     //33333333333333
     public static void t1() {
