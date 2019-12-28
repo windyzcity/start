f95480b7921a6213676d8201ed9fb906a9eb4a6c
xklieren <965994058@qq.com>
2019-12-28 15:36:56 +0800

    commit t1

--- start/src/main/java/controller/FileController.java
+++ start/src/main/java/controller/FileController.java
@@ -4,4 +4,8 @@ public class FileController {
     public static void main(String[] args) {
         System.out.println("20191228");
     }
+
+    public static void t1(String[] args) {
+        System.out.println("creat by master");
+    }
 }
