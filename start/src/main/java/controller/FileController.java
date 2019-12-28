08ae8f38106d6b7065650edff40b967aa68a397b
windyzcity <49401504+windyzcity@users.noreply.github.com>
2019-12-28 15:26:49 +0800

    Update FileController.java
    update

--- start/src/main/java/controller/FileController.java
+++ start/src/main/java/controller/FileController.java
@@ -4,4 +4,7 @@ public class FileController {
     public static void main(String[] args) {
         System.out.println("20191228");
     }
+    public static void t222(String[] args) {
+        System.out.println("create by test20191228");
+    }
 }
